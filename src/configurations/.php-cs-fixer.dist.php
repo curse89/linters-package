3f8f6870c0f5926a036cb1dc31f80555be907ee6
Кривич Сергей <krivich@retailcrm.ru>
2022-05-27 12:22:30 +0300

    Composer dependecies && linters configs

---
+++ src/configurations/.php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude(['var', 'vendor'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'concat_space' => ['spacing' => 'one'],
+    ])
+    ->setRiskyAllowed(false)
+    ->setFinder($finder)
+    ->setCacheFile('var/cache/.php-cs-fixer.cache') // forward compatibility with 3.x line
+;
